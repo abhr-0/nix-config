bd9c5a90281885387a237baf49221baf8de532bd
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-04-25 13:01:13 +0530

    Fix how sops-nix is imported in home-manager

--- home-manager/abhro/sops.nix
+++ home-manager/abhro/sops.nix
@@ -1,6 +1,7 @@
 { config, inputs, ... }:
 {
-  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+  # Use imports instead when using home-manager as standalone
+  # imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
   sops = {
     age.keyFile = "/home/abhro/.config/sops/age/keys.txt";
--- nixosModules/settings/home-manager.nix
+++ nixosModules/settings/home-manager.nix
@@ -4,6 +4,8 @@
 
   home-manager = {
     useGlobalPkgs = true;
+    # Use sharedModules instead of imports when using home-manager as a NixOS module
+    sharedModules = [ inputs.sops-nix.homeManagerModules.sops ];
     extraSpecialArgs = { inherit inputs; };
     users.abhro = ../../home-manager/abhro/home.nix;
   };
