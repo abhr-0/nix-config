1d716f4f35729359e4bc8a11ecc2df71850a6f9c
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-04-25 19:12:40 +0530

    Use age keyfile for sops in nixos

--- nixosModules/security/sops.nix
+++ nixosModules/security/sops.nix
@@ -6,6 +6,11 @@
     defaultSopsFile = "${inputs.nix-secrets}/secrets/host.yaml";
     defaultSopsFormat = "yaml";
 
+    age = {
+      keyFile = "/var/lib/sops-nix/age/keys.txt";
+      # generateKey = true;
+    };
+
     secrets = {
       abhro-password.neededForUsers = true;
       usbguard_rules_file = { };
