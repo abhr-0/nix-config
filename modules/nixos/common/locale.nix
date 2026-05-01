dbd7e01df35e314f821d9d2a29e21f69b4e0418b
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-05-01 17:37:36 +0530

    Remove multilingual input

--- modules/nixos/common/locale.nix
+++ modules/nixos/common/locale.nix
@@ -1,14 +1,6 @@
-{ pkgs, ... }:
 {
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocales = [ "en_IN/UTF-8" ];
-
-    # Enable ibus for multi-lingual input
-    inputMethod = {
-      enable = true;
-      type = "ibus";
-      ibus.engines = [ pkgs.ibus-engines.m17n ]; # no input setup ?
-    };
   };
 }
