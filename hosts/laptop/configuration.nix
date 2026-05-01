a461eef2a56d6608a578e05aed453a40af799a39
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-05-01 16:21:24 +0530

    Add ssd config for host laptop from nixos-hardware

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,6 +4,7 @@
     # Import common hardware configs from nixos-hardware
     inputs.hardware.nixosModules.common-cpu-amd
     inputs.hardware.nixosModules.common-gpu-amd
+    inputs.hardware.nixosModules.common-pc-ssd
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
