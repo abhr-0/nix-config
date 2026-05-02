5bb32f0405ea4d58581195db0106e6c467118e7a
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-05-02 12:45:49 +0530

    Remove redundant file modules/flake/default.nix as all files in modules/* are imported using import-tree

--- modules/flake/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  imports = [
-    ./nixos.nix
-    ./pre-commit.nix
-    ./shell.nix
-    # ./home-manager.nix # Not used currently, as home-manager is being used as a nixos module. Can be enabled if standalone home-manager configuration is needed in the future.
-  ];
-}
