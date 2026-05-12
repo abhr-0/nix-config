dec907895a5ab3c98e2f12acbf377d1da299e535
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-05-12 16:06:01 +0530

    Setup keybinds for gitsigns

--- modules/nixvim/plugins/git.nix
+++ modules/nixvim/plugins/git.nix
@@ -1,32 +1,105 @@
+{ lib, ... }:
 {
   config = {
     plugins = {
-      gitsigns.enable = true; # Toggle cuurent line blame? and more config
+      gitsigns.enable = true;
       lazygit.enable = true;
-      # diffview.enable = true;
-      # neogit
     };
 
-    keymaps = [
-      {
-        mode = "n";
-        action = "<cmd>Gitsigns next_hunk<CR>";
-        key = "]h";
-        options.desc = "Next Hunk";
-      }
-      {
-        mode = "n";
-        action = "<cmd>Gitsigns prev_hunk<CR>";
-        key = "[h";
-        options.desc = "Prev Hunk";
-      }
-      # TODO: stage, reset, preview hunks (stage reset selected range)
-      {
-        mode = "n";
-        key = "<leader>g";
-        action = "<CMD>LazyGit<CR>";
-        options.desc = "Open LazyGit";
-      }
-    ];
+    keymaps =
+      let
+        gitsigns = "require('gitsigns')";
+      in
+      [
+        {
+          mode = "n";
+          action = lib.nixvim.mkRaw ''
+            function()
+              if vim.wo.diff then
+                vim.cmd.normal({']c'})
+              else
+                ${gitsigns}.nav_hunk('next')
+              end
+            end
+          '';
+          key = "]h";
+          options.desc = "Next Hunk";
+        }
+        {
+          mode = "n";
+          action = lib.nixvim.mkRaw ''
+            function()
+              if vim.wo.diff then
+                vim.cmd.normal({'[c'})
+              else
+                ${gitsigns}.nav_hunk('prev')
+              end
+            end
+          '';
+          key = "[h";
+          options.desc = "Prev Hunk";
+        }
+        {
+          mode = "n";
+          action = lib.nixvim.mkRaw "${gitsigns}.stage_hunk";
+          key = "<leader>hs";
+          options.desc = "Stage Hunk";
+        }
+        {
+          mode = "n";
+          action = lib.nixvim.mkRaw "${gitsigns}.reset_hunk";
+          key = "<leader>hr";
+          options.desc = "Reset Hunk";
+        }
+        {
+          mode = "v";
+          action = lib.nixvim.mkRaw "function() ${gitsigns}.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end";
+          key = "<leader>hs";
+          options.desc = "Stage Hunk Selected";
+        }
+        {
+          mode = "v";
+          action = lib.nixvim.mkRaw "function() ${gitsigns}.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end";
+          key = "<leader>hr";
+          options.desc = "Reset Hunk Selected";
+        }
+        {
+          mode = "n";
+          action = lib.nixvim.mkRaw "${gitsigns}.preview_hunk";
+          key = "<leader>hp";
+          options.desc = "Preview Hunk"; # Use inline?
+        }
+        {
+          mode = "n";
+          action = lib.nixvim.mkRaw "function() ${gitsigns}.blame_line({ full = true }) end"; # Full needed?
+          key = "<leader>hb";
+          options.desc = "Blame Line";
+        }
+        {
+          mode = "n";
+          action = lib.nixvim.mkRaw ''
+            function()
+              if vim.wo.diff then
+                vim.cmd("only")
+                vim.cmd("diffoff")
+              else
+                if vim.tbl_isempty(${gitsigns}.get_hunks()) then
+                  vim.notify("No Git changes in current buffer", vim.log.levels.INFO)
+                else
+                  ${gitsigns}.diffthis()
+                end
+              end
+            end
+          '';
+          key = "<leader>hd";
+          options.desc = "Diff Current Buffer";
+        }
+        {
+          mode = "n";
+          key = "<leader>g";
+          action = "<CMD>LazyGit<CR>";
+          options.desc = "Open LazyGit";
+        }
+      ];
   };
 }
