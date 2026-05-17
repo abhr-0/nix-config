e8410a6641f2ab8ce326b801cb083891f29fc234
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-05-17 17:17:45 +0530

    Add CopilotChat.nvim to nixvim

--- modules/nixvim/plugins/copilot.nix
+++ modules/nixvim/plugins/copilot.nix
@@ -1,14 +1,36 @@
 {
-  config.plugins = {
-    copilot-lua = {
-      enable = true;
-      settings = {
-        panel.enabled = false;
-        suggestion = {
-          enabled = true;
-          auto_trigger = true;
+  config = {
+    plugins = {
+      copilot-lua = {
+        enable = true;
+        settings = {
+          panel.enabled = false;
+          suggestion = {
+            enabled = true;
+            auto_trigger = true;
+          };
+        };
+      };
+      copilot-chat = {
+        enable = true;
+        settings = {
+          auto_insert_mode = true;
+          headers = {
+            assistant = " Copilot";
+            tool = " Tool";
+            user = " User";
+          };
+          window.width = 0.3;
         };
       };
     };
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>c";
+        action = ":CopilotChatToggle<CR>";
+        options.desc = "Toggle Copilot Chat";
+      }
+    ];
   };
 }
