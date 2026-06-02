e46b4a601ef4c92892894c4dd473ff06433e869a
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-06-02 11:03:38 +0530

    Fix typo in repo owner's name for overlay

--- flake/overlays.nix
+++ flake/overlays.nix
@@ -17,7 +17,7 @@
         blink-cmp-copilot-chat = final.vimUtils.buildVimPlugin {
           name = "blink-cmp-copilot-chat";
           src = final.fetchFromGitHub {
-            owner = "prxwg";
+            owner = "pxwg";
             repo = "blink-cmp-copilot-chat";
             rev = "f6b88c9a0afa3080ab3157a630717a0962533185";
             hash = "sha256-J5/fYG06WNl5ykH2K9RDOeZQ1zkZHpsKld4Vyl8TCAI=";
