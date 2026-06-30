b163b00277c04c4f21354b3d82f464acc37a81b7
abhr-0 <121384410+abhr-0@users.noreply.github.com>
2026-06-30 14:05:18 +0530

    Remove CopilotChat.nvim override in overlay as upstream has fix

--- flake/overlays.nix
+++ flake/overlays.nix
@@ -1,18 +1,8 @@
 {
   flake.overlays.default = final: prev: {
     vimPlugins = prev.vimPlugins.extend (
-      _: prev': {
-        # TODO: Remove when fix is merged upstream
-        CopilotChat-nvim = prev'.CopilotChat-nvim.overrideAttrs (_: {
-          src = final.fetchFromGitHub {
-            owner = "CopilotC-Nvim";
-            repo = "CopilotChat.nvim";
-            rev = "2025f92022bef434de0511c5157a19974a9377a4";
-            hash = "sha256-YyzJBoOVYefBlpck9O80BdPjQIlH6p5o6Ta2n9aWu4A=";
-          };
-          version = "local";
-        });
-
+      _: _: {
+        # final' and prev'
         # Not in nixpkgs
         blink-cmp-copilot-chat = final.vimUtils.buildVimPlugin {
           name = "blink-cmp-copilot-chat";
