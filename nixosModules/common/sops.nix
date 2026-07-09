82e05563cfa9bd30709873ffa843e924577bc1ed
Abhraneel Mukherjee <121384410+abhr-0@users.noreply.github.com>
2026-07-09 23:31:44 +0530

    Use raw path instead of string for sops module in nixos

--- nixosModules/common/sops.nix
+++ nixosModules/common/sops.nix
@@ -3,7 +3,7 @@
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   sops = {
-    defaultSopsFile = "../../secrets/host.yaml";
+    defaultSopsFile = ../../secrets/host.yaml;
     defaultSopsFormat = "yaml";
 
     # Disable automatic importing of SSH host RSA keys as GPG keys for sops
